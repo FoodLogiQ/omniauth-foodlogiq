335d2b7df233dc059a876fb8583712edcc7c9c86
Shaun Pack <kaizenpack@gmail.com>
2012-11-14 17:08:17 -0500

    removed some extra fields

--- lib/omniauth/strategies/foodlogiq.rb
+++ lib/omniauth/strategies/foodlogiq.rb
@@ -3,6 +3,8 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class FoodLogiQ < OmniAuth::Strategies::OAuth2
+      option :name, "foodlogiq"
+
       option :client_options, {
         :site => 'http://localhost:3000',
         :authorize_url => 'http://localhost:3000/oauth/authorize',
@@ -17,14 +19,8 @@ module OmniAuth
 
       info do
         {
-          'nickname' => raw_info['login'],
-          'email' => email,
-          'name' => raw_info['name'],
-          'image' => raw_info['avatar_url'],
-          'urls' => {
-            'GitHub' => "https://github.com/#{raw_info['login']}",
-            'Blog' => raw_info['blog'],
-          },
+          'email' => raw_info['email'],
+          'name' => raw_info['name']
         }
       end
 
@@ -33,23 +29,8 @@ module OmniAuth
       end
 
       def raw_info
-        access_token.options[:mode] = :query
         @raw_info ||= access_token.get('/user').parsed
       end
-
-      def email
-        raw_info['email'] || (email_access_allowed? ? emails.first : nil)
-      end
-
-      def emails
-        access_token.options[:mode] = :query
-        @emails ||= access_token.get('/user/emails').parsed
-      end
-
-      def email_access_allowed?
-        options['scope'] && !(options['scope'] == 'public')
-      end
-
     end
   end
 end
