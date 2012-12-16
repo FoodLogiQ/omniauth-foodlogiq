3d5da21d85c55e661c9b16796a91e53af90ab41f
Shaun Pack <kaizenpack@gmail.com>
2012-12-16 15:42:43 -0500

    bundle update and removed tests that didn't actually test anything - more tests should be added here

--- spec/omniauth/strategies/foodlogiq_spec.rb
+++ spec/omniauth/strategies/foodlogiq_spec.rb
@@ -5,18 +5,6 @@ describe OmniAuth::Strategies::FoodLogiQ do
     OmniAuth::Strategies::FoodLogiQ.new({})
   end
 
-  context "client options" do
-    it 'should have correct site' do
-      subject.options.client_options.site.should eq("http://localhost:3000")
-    end
-
-    it 'should have correct authorize url' do
-      subject.options.client_options.authorize_url.should eq('http://localhost:3000/oauth/authorize')
-    end
-
-    it 'should have correct token url' do
-      subject.options.client_options.token_url.should eq('http://localhost:3000/oauth/access_token')
-    end
-  end
+  it "should have some tests here"
 
 end
