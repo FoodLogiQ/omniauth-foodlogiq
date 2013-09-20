191588371af2f84b669409d5711667dcab750034
David Johnson <djak250@gmail.com>
2013-09-20 14:57:10 -0400

    Ready to be updated

--- lib/omniauth/strategies/foodlogiq.rb
+++ lib/omniauth/strategies/foodlogiq.rb
@@ -27,7 +27,34 @@ module OmniAuth
         @raw_info ||= access_token.get('/user').parsed
       end
     end
+    class FoodLogiQRegister < OmniAuth::Strategies::OAuth2
+      option :name, "foodlogiqRegister"
+
+      option :client_options, {
+        :site => ENV['FOODLOGIQ_SITE'],
+        :authorize_path => '/oauth/authorize',
+        :token_url => '/oauth/access_token',
+        :register => true
+      }
+
+      uid do 
+        raw_info['email']
+      end
+
+      info do
+        {
+          'email' => raw_info['email'],
+          'firstName' => raw_info['firstName'],
+          'lastName' => raw_info['lastName']
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/user').parsed
+      end
+    end
   end
 end
 
 OmniAuth.config.add_camelization 'foodlogiq', 'FoodLogiQ'
+OmniAuth.config.add_camelization 'foodlogiqRegister', 'FoodLogiQRegister'
